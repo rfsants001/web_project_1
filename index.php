335a642b31f9add46099ef7ce596d60273a7cadb
Rafael Santos <rf.sants001@gmail.com>
2021-07-01 21:02:25 -0300

    header and assets

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Project 1</title>
+</head>
+<body>
+    <section class="main">
+        <div class="layer"></div>
+        <div class="center">
+            <header>
+                <div class="icons-social">
+                    <a href="https://facebook.com"><i class="fa fa-facebook"></i></a>
+                    <a href="https://instagram.com"><i class="fa fa-instagram"></i></a>
+                </div>
+                <div class="clear"></div>
+                <div class="logo">Logomark</div>
+                <nav class="desktop">
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Contact</a></li>
+                    </ul>
+                </nav>
+                <div class="clear"></div>
+            </header>
+       </div>
+    </section>
+</body>
+</html>
