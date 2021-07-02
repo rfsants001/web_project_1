892021fdc182146c18bc12280e8c6eb4d2bc2a80
Rafael Santos <rf.sants001@gmail.com>
2021-07-01 23:00:44 -0300

    Add favicon and products section

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -8,8 +9,10 @@
     <link rel="stylesheet" href="css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <title>Project 1</title>
 </head>
+
 <body>
     <section class="main">
         <div class="layer"></div>
@@ -20,7 +23,7 @@
                     <a href="https://instagram.com"><i class="fa fa-instagram"></i></a>
                 </div>
                 <div class="clear"></div>
-                <div class="logo">Logomark</div>
+                <div class="logo">Project 1</div>
                 <nav class="desktop">
                     <ul>
                         <li><a href="">Home</a></li>
@@ -30,7 +33,71 @@
                 </nav>
                 <div class="clear"></div>
             </header>
-       </div>
+
+            <div class="header-content">
+                <h2>Welcome to our <span>Website</span></h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ullamcorper ultrices justo, in volutpat arcu tristique eu. Nulla tincidunt placerat erat ac ullamcorper. Etiam maximus varius sagittis. Suspendisse sed viverra sem, in lacinia est. Fusce auctor lectus quis condimentum ultricies. Mauris porta euismod dolor, non vulputate lacus posuere semper. Nullam lacinia mauris auctor dignissim condimentum.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ullamcorper ultrices justo, in volutpat arcu tristique eu. Nulla tincidunt placerat erat ac ullamcorper. Etiam maximus varius sagittis. Suspendisse sed viverra sem, in lacinia est. Fusce auctor lectus quis condimentum ultricies. Mauris porta euismod dolor, non vulputate lacus posuere semper. Nullam lacinia mauris auctor dignissim condimentum.</p>
+                <a href=""> Learn more </a>
+            </div>
+        </div>
+    </section>
+
+    <section class="plans">
+        <div class="header-plans">
+            <div class="center">
+                <h2>Check Out our plans</h2>
+            </div>
+        </div>
+        <section class="box-plans">
+            <div class="center">
+                <div class="single-box-plans">
+                    <div class="wraper-box-plans">
+                        <h2>Lorem ipsum</h2>
+                        <div class="benefit-plan">
+                            <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                            <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                            <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                            <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                        </div>
+                        <div class="price">
+                            <h2>$50,00</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="single-box-plans">
+                    <div class="wraper-box-plans">
+                        <h2>Lorem ipsum</h2>
+                        <div class="benefit-plan">
+                            <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                            <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                            <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                            <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                        </div>
+                        <div class="price">
+                            <h2>$150,00</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="single-box-plans">
+                    <div class="wraper-box-plans">
+                        <h2>Lorem ipsum</h2>
+                        <div class="benefit-plan">
+                            <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                            <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                            <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                            <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                        </div>
+                        <div class="price">
+                            <h2>$250,00</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </section>
 </body>
+
 </html>
